b11ccefc32b6b537e064e196d9e0240e1fada2d7
JamalNizami <jamal.anjum2001@gmail.com>
2023-01-19 11:07:46 +0500

    initial commit

--- application/views/includes/footer/default_footer.php
+++ application/views/includes/footer/default_footer.php
@@ -1,3 +1,4 @@
 <script type="text/javascript" src="<?= base_url('assets/js/custom.js') ?>" ></script>
+<script type="text/javascript" src="<?= base_url('assets/js/bootstrap.min.js') ?>" ></script>
 </body>
 </html>
